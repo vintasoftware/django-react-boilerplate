6b727c08045e8d321f96829b88d4888a0b05f0f6
Flávio Juvenal <flaviojuvenal@gmail.com>
2016-08-29 14:41:55 -0300

    Adding missing Whitenoise on wsgi.py

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -4,13 +4,16 @@ WSGI config for {{project_name}} project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.production")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
