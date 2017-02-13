d40c0f2e4d5a8c26b91288397fa9be9f17ff4156
anderson <anderson@vinta.com.br>
2017-02-13 18:20:46 -0300

    Added celery namespace

--- project_name/celery.py
+++ project_name/celery.py
@@ -10,5 +10,5 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.local")
 
 app = Celery('{{project_name}}_tasks')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
