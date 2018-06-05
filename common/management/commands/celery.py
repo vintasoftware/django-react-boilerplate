63ebc6e04c226f7a192b978b4760a38d618e0441
Carlos Coêlho <carlos@vinta.com.br>
2018-06-05 15:36:01 -0300

    Add celery management command with hot-reload support
    Signed-off-by: Carlos Coêlho <carlos@vinta.com.br>

---
+++ common/management/commands/celery.py
@@ -0,0 +1,21 @@
+import shlex
+from subprocess import PIPE  # nosec
+
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+import psutil
+
+
+def restart_celery():
+    for proc in psutil.process_iter():
+        if proc.name() == 'celery':
+            proc.kill()
+    cmd = "celery worker -A {{project_name}} -l INFO"
+    psutil.Popen(shlex.split(cmd), stdout=PIPE)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        print('Starting celery worker with autoreload')
+        autoreload.main(restart_celery)
