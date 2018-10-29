2b30519ddc55d207bb0ea446ed5ebd607165a746
Carlos Coêlho <carlos@vinta.com.br>
2018-10-29 17:39:21 -0300

    Moved jest configuration from package.json to jest.config.js
    Signed-off-by: Carlos Coêlho <carlos@vinta.com.br>

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,37 @@ module.exports = {
     '^.+\\.jsx$': 'babel-jest',
     '^.+\\.js$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [
+    'node_modules/*',
+  ],
+  modulePaths: [
+    'assets',
+    'assets/js',
+    'assets/js/app',
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+  ],
+  setupFiles: [
+    './jest-setup.js',
+  ],
+  collectCoverageFrom: [
+    'assets/js/**/*.{js,jsx}',
+  ],
+  coveragePathIgnorePatterns: [
+    'assets/js/store.js',
+    'assets/js/index.js',
+    'assets/js/jquery-index.js',
+    'assets/js/constants/*',
+    'assets/js/pages/*',
+    'assets/js/tests/*',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 10,
+    },
+  },
 };
