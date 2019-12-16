ee69ddf9fab513eade7242e8ecb33aed166d74ce
Joao Veras <jhveras@motorola.com>
2019-12-16 08:17:05 -0300

    Remove colorchange component references from home component

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 
-import ColorChanger from '../app/example-app';
-
 const Home = () => {
   const [showBugComponent, setShowBugComponent] = useState(false);
 
   return (
     <>
-      <ColorChanger />
       <button type="button" onClick={() => setShowBugComponent(true)}>
         Click to test if Sentry is capturing frontend errors! (Should only work in Production)
       </button>
