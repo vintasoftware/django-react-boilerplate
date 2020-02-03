4d7c25333539b2b541de26d4f56b8eb994b8b4cc
Vanessa Barreiros <vanessa@vinta.com.br>
2020-02-02 21:01:02 -0300

    Add pyproject.toml and run black on backend folder

--- backend/project_name/celerybeat_schedule.py
+++ backend/project_name/celerybeat_schedule.py
@@ -3,8 +3,5 @@ from celery.schedules import crontab
 
 CELERYBEAT_SCHEDULE = {
     # Internal tasks
-    'clearsessions': {
-        'schedule': crontab(hour=3, minute=0),
-        'task': 'users.tasks.clearsessions'
-    },
+    "clearsessions": {"schedule": crontab(hour=3, minute=0), "task": "users.tasks.clearsessions"},
 }
--- backend/users/managers.py
+++ backend/users/managers.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **kwargs):
         email = self.normalize_email(email)
         user = self.model(email=email, **kwargs)
