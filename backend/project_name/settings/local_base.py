7b27423bef813befe1bb9dd5cb14843d847bff42
Joao Veras <jhveras@motorola.com>
2020-02-24 18:34:20 -0300

    Fix mailhog settings

--- backend/project_name/settings/local_base.py
+++ backend/project_name/settings/local_base.py
@@ -26,13 +26,10 @@ AUTH_PASSWORD_VALIDATORS = []  # allow easy passwords only on local
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
 
-# Email
+# Email settings for mailhog
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'mailhog'
+EMAIL_PORT = 1025
 
 # Logging
 LOGGING = {
