6dcab2012a50aef249ef64c161c53f4a3b20cca4
rvlb-19 <renatovlbarros19@gmail.com>
2021-03-23 13:57:50 -0300

    Remove app_dirs from templates settings

--- backend/project_name/settings/base.py
+++ backend/project_name/settings/base.py
@@ -56,7 +56,6 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [base_dir_join("templates")],
-        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
