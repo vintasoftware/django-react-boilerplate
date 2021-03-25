9881a3eed3a64d6c1bbd87922dd2490ec4cc66c5
rvlb-19 <renatovlbarros19@gmail.com>
2021-03-25 11:55:22 -0300

    Fix frontend images serving

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
+import DjangoImgSrc from '../../assets/images/django-logo-negative.png';
+
 const Home = () => {
   const [showBugComponent, setShowBugComponent] = useState(false);
 
   return (
     <>
+      <div id="django-background">
+        If you're seeing the green Django logo on a white background and this
+        text color is #092e20, frontend static files serving is working
+      </div>
+      <div id="django-logo-wrapper">
+        <div>Below this text, you should see an img tag with the white Django logo on a green background</div>
+        <img src={DjangoImgSrc} alt="Django Negative Logo" />
+      </div>
       <Button variant="outline-dark" onClick={() => setShowBugComponent(true)}>
         Click to test if Sentry is capturing frontend errors! (Should only work in Production)
       </Button>
