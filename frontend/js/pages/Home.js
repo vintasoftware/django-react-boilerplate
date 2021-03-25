18d088b78320f9db7234350d823ce2aa651d6912
rvlb-19 <renatovlbarros19@gmail.com>
2021-03-25 12:04:58 -0300

    Fix linter

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -9,12 +9,15 @@ const Home = () => {
   return (
     <>
       <div id="django-background">
-        If you're seeing the green Django logo on a white background and this
-        text color is #092e20, frontend static files serving is working
+        If you are seeing the green Django logo on a white background and this text color is
+        #092e20, frontend static files serving is working
       </div>
       <div id="django-logo-wrapper">
-        <div>Below this text, you should see an img tag with the white Django logo on a green background</div>
-        <img src={DjangoImgSrc} alt="Django Negative Logo" />
+        <div>
+          Below this text, you should see an img tag with the white Django logo on a green
+          background
+        </div>
+        <img alt="Django Negative Logo" src={DjangoImgSrc} />
       </div>
       <Button variant="outline-dark" onClick={() => setShowBugComponent(true)}>
         Click to test if Sentry is capturing frontend errors! (Should only work in Production)
