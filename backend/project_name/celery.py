81d9e2fcbc98e4ce0ac36bdd4fd4db33d89baa78
rvlb-19 <renatovlbarros19@gmail.com>
2021-03-25 13:57:55 -0300

    Update celery.py

--- backend/project_name/celery.py
+++ backend/project_name/celery.py
@@ -1,13 +1,22 @@
 import os
+import sys
 
 from django.apps import apps
 
 from celery import Celery
+from decouple import config
 
 from .celerybeat_schedule import CELERYBEAT_SCHEDULE
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.local")
+settings_module = config("DJANGO_SETTINGS_MODULE", default=None)
+if settings_module is None:
+    print(
+        "Error: no DJANGO_SETTINGS_MODULE found. Will NOT start devserver. "
+        "Remember to create .env file at project root. "
+        "Check README for more info."
+    )
+    sys.exit(1)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 app = Celery("{{project_name}}_tasks")
 app.config_from_object("django.conf:settings", namespace="CELERY")
