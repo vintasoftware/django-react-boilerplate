6a6584127249477d137cb979ea10bf69ebdb3129
rvlb <renatovlbarros19@gmail.com>
2021-06-11 15:51:06 -0300

    Fix test DB config

--- backend/project_name/settings/test.py
+++ backend/project_name/settings/test.py
@@ -3,10 +3,6 @@ from .base import *  # noqa
 
 SECRET_KEY = "test"
 
-DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": base_dir_join("db.sqlite3"),}
-}
-
 STATIC_ROOT = base_dir_join('staticfiles')
 STATIC_URL = '/static/'
 
