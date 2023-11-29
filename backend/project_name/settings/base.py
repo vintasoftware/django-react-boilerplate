cbaafdaa8c7e984f74d549eb896d232f47d6c302
Flávio Juvenal <flavio@vinta.com.br>
2023-11-29 15:42:50 -0300

    Small fix on regex of WEBPACK_LOADER

--- backend/project_name/settings/base.py
+++ backend/project_name/settings/base.py
@@ -128,7 +128,7 @@ WEBPACK_LOADER = {
         "CACHE": False,  # on DEBUG should be False
         "STATS_FILE": base_dir_join("../webpack-stats.json"),
         "POLL_INTERVAL": 0.1,
-        "IGNORE": [".+\.hot-update.js", ".+\.map"],
+        "IGNORE": [r".+\.hot-update.js", r".+\.map"],
     }
 }
 
